5bab299be43b9064a588da2ce8f8ba6f68bf741b
Abay Ibrayev <abayibrayev7@gmail.com>
2021-09-08 18:38:48 +0100

    setting up hooks in the chat component

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,18 +9,22 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-
 const io = socketio(server);
+
+app.use(cors());
+//calling a router as a middleware
+app.use(router);
 
 io.on('connection', (socket) => {
   console.log('We have a new connection!');
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+  });
 
   socket.on('disconnect', () => {
     console.log('User left!');
   });
 });
-
-//calling a router as a middleware
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
